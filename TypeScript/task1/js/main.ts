5e03f8ed53835b0979db170a00066e14b4e09ea2
haidarhalessa <haidarhalessa@gmail.com>
2025-09-14 10:29:33 +0300

    task3

--- TypeScript/task1/js/main.ts
+++ TypeScript/task1/js/main.ts
@@ -11,12 +11,10 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
-console.log(director1);
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}`;
+}
