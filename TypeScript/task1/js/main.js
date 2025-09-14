8255c262322e228eaab3b22a1b9d939c5b046447
haidarhalessa <haidarhalessa@gmail.com>
2025-09-14 10:35:32 +0300

    task4

--- TypeScript/task1/js/main.js
+++ TypeScript/task1/js/main.js
@@ -1,9 +1,24 @@
 #! /usr/bin/env node
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StudentClass = exports.printTeacher = void 0;
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
 };
-console.log(director1);
+exports.printTeacher = printTeacher;
+exports.StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working on homework';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+var student = new exports.StudentClass('John', 'Doe');
+console.log(student.workOnHomework());
+console.log(student.displayName());
--- TypeScript/task1/js/main.ts
+++ TypeScript/task1/js/main.ts
@@ -18,3 +18,31 @@ interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName[0]}. ${lastName}`;
 }
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export const StudentClass: StudentClassConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working on homework';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
